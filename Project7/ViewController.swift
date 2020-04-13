ff552a1ee35a5d103e68bbd165b8eb9f399ab1a2
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-13 13:47:03 +0900

    added functionality of filtering petitions by keyword

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +18,8 @@ class ViewController: UITableViewController {
         let urlString: String
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterPetitions))
 
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -35,12 +38,12 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -51,13 +54,14 @@ class ViewController: UITableViewController {
 
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
     
@@ -71,6 +75,31 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         present(ac, animated: true)
+    }
+    
+    @objc func filterPetitions() {
+        let ac = UIAlertController(title: "Filter by keyword:", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default, handler: { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.applyFilter(answer)
+        })
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func applyFilter(_ answer: String) {
+        filteredPetitions = []
+        
+        for petition in petitions {
+            if petition.title.contains(answer) || petition.body.contains(answer) {
+                filteredPetitions.append(petition)
+            }
+        }
+        
+        tableView.reloadData()
     }
 }
 
